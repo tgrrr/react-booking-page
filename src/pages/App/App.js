659e62de79bfedc935a01f1151f18fa9579ce66b
Phil Steinke <phil@tgrrr.com>
2019-12-10 21:28:07 +1100

    clean up CRA

---
+++ src/pages/App/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="Header">
+        Header
+      </header>
+
+      <nav className="Nav">
+        Nav
+      </nav>
+
+      <main className="Main">
+        <section className="Main__Section">
+          Section 1
+        </section>
+        <section className="Main__Section">
+          Section 2
+        </section>
+        <section className="Main__Section">
+          Section 3
+        </section>
+      </main>
+
+      <footer className="Footer">
+        Footer  
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/App/index.js
@@ -0,0 +1,3 @@
+import App from 'src/pages/App/App';
+
+export default App;
